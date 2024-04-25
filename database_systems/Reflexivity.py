0d1d6d832ed8a4f6dfa6f092715359f3c8814673
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2024-04-25 17:09:52 -0500

    Add files via upload

---
+++ database_systems/Reflexivity.py
@@ -0,0 +1,7 @@
+def reflexivity(dependency):
+    #If  ϐ⊆ α, then α->ϐ 
+    #This is pretty much asking for me to find all the 
+    #Subsets of each right side
+    beta =  []
+    deps = #Extract dependencies somehow
+    def decisionTree(i):
