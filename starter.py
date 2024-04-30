76132b447f34a626af4ba69c33fadcd3c918c7de
Arthur Viegas Eguia <95381448+Arthur-Viegas-Eguia@users.noreply.github.com>
2024-04-30 12:27:35 -0500

    Creating a closure

--- starter.py
+++ starter.py
@@ -49,11 +49,24 @@ class relnset(set):
 
 
 def closure(attrs: atset, fds: fdset) -> fdset:
-    '''Given a set of attributes (atset) and a set of functional dependencies
-    (fdset), produce a new set of functional dependencies (fdset) that
-    represents the closure of the original fdset.'''
+    result = fds
+    # reflexivityDeps = reflexivity(attrs)
+    # for i in reflexivityDeps:
+    #     result.add(i)
 
-    return fds
+    oldResult = fdset()
+    while result != oldResult:
+        oldResult = result.copy()
+
+        augResults = augmentation(attrs, result)
+        for i in augResults:
+            result.add(i)
+            
+        transitiveDeps = transitivity(result)
+        for i in transitiveDeps:
+            result.add(i)
+
+    return result
 
 
 def bcnf(reln, fds) -> relnset:
@@ -65,24 +78,119 @@ def bcnf(reln, fds) -> relnset:
     result.add(reln)
     return result
 
+#Gets all of the possible subsets from a series of attributes
+def subsets(attributes):
+    '''Copied (and slightly adapted) from https://neetcode.io/practice
+    Watched the video at https://www.youtube.com/watch?v=REOH22Xwdkk&ab_channel=NeetCode
+    to understand the logic. All the code was design and mostly written by Neetcode
+    '''
+    #This is our powerset (i.e.) all subsets of our attributes
+    powerset = []
 
-def main() -> None:
-    attributes = atset([1, 2])
-    print("Some attributes:", attributes)
+    subsets = []
 
+    #This is a recursive DFS that goes through all possibilities of
+    #Including and not including each attribute through a decision 
+    #tree Putting it into our result
+    def decisionTree(i):
+        if i >= len(attributes):
+            #Prevents from adding the empty array
+            if len(subsets) != 0:
+                powerset.append(subsets.copy())
+            return
+        # decision to include nums[i]
+        subsets.append(attributes[i])
+        decisionTree(i + 1)
+        # decision NOT to include nums[i]
+        subsets.pop()
+        decisionTree(i + 1)
+
+    decisionTree(0)
+    return powerset
+
+def reflexivity(attributes):
+    powerset = subsets(attributes)
+    alpha = set()
+    for i in powerset:
+        alpha.add(frozenset(i))
+    beta = alpha.copy()
     fds = fdset()
-    fds.add(fd(atset([1]), atset([2])))
-    fds.add(fd(atset([2]), atset([1])))
-    print("Some fds:")
-    print(fds)
+    for b in beta:
+        for a in alpha:
+            if b.issubset(a):
+                fds.add(fd(atset(a), atset(b)))
+    return fds
 
-    result = closure(attributes, fds)
-    print("A closure:")
-    print(result)
+def augmentation(attributes, deps):
+    gammas = subsets(attributes)
+    fds = fdset()
+    for gamma in gammas:
+        for dep in deps:
+            Right_side = atset(gamma + list(dep.RHS))
+            Left_side = atset(gamma + list(dep.LHS))
+            fds.add(fd(Left_side, Right_side))
+    return fds
 
-    decomp_result = bcnf(attributes, fds)
-    print("A decomposition:", decomp_result)
+def transitivity(deps):
+    fds = fdset()
+    fdsLenOld = -1
+    while fdsLenOld != len(fds):
+        fdsLenOld = len(fds)
+        for beta in deps:
+            for alpha in deps:
+                right = beta.RHS
+                left = alpha.LHS
+                if right == left:
+                    fds.add(fd(beta.LHS, alpha.RHS))
+        for i in fds:
+            deps.add(i)
+    return fds
 
 
-if __name__ == "__main__":
-    main()
+
+
+            
+
+
+    
+
+
+# def main() -> None:
+#     attributes = atset([1, 2])
+#     print("Some attributes:", attributes)
+
+#     fds = fdset()
+#     fds.add(fd(atset([1]), atset([2])))
+#     fds.add(fd(atset([2]), atset([1])))
+#     print("Some fds:")
+#     print(fds)
+
+#     result = closure(attributes, fds)
+#     print("A closure:")
+#     print(result)
+
+#     decomp_result = bcnf(attributes, fds)
+#     print("A decomposition:", decomp_result)
+
+
+# if __name__ == "__main__":
+#     main()
+
+attrs = [1, 2]
+fds = fdset()
+fds.add(fd(atset([1]), atset([2])))
+fds.add(fd(atset([2]), atset([3])))
+fds.add(fd(atset([3]), atset([4])))
+a = closure(attrs, fds)
+for asd in a:
+    print(asd)
+
+
+# 1->2
+# 2->3
+# 3->4
+# 1->3
+# 1->4
+# 2->4
+
+
